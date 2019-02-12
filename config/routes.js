292760cf391a4b1a2bde783525cdb9903026f885
Lambda_School_Loaner_26 <marguel.gtz@gmail.com>
2019-02-11 19:48:42 -0600

    finished endpoints

--- config/routes.js
+++ config/routes.js
@@ -1,6 +1,9 @@
+require('dotenv').config()
 const axios = require('axios');
+const bcrypt = require('bcryptjs')
+const db = require('../database/dbConfig')
 
-const { authenticate } = require('../auth/authenticate');
+const { authenticate, generateToken } = require('../auth/authenticate');
 
 module.exports = server => {
   server.post('/api/register', register);
@@ -10,10 +13,64 @@ module.exports = server => {
 
 function register(req, res) {
   // implement user registration
+  const creds = req.body
+  const hash = bcrypt.hashSync(creds.password, 12)
+  creds.password = hash
+  if(creds.username && creds.password) {
+    db.register(creds)
+      then(ids => {
+        const id = ids[0]
+        db.getUser(id)
+          .then(user => {
+            const token = generateToken(user)
+            res
+              .status(201)
+              .json({token})
+          })
+          .catch(() => {
+            res
+              .status(500)
+              .json({message: 'Failed to Authenticate'})
+          })
+      })
+      .catch(() => {
+        res
+          .status(500)
+          .json({message: 'Failed to register'})
+      })
+  } else {
+    res
+      .status(404)
+      .json({message: 'username/password Missing'})
+  }
 }
 
 function login(req, res) {
   // implement user login
+  const creds = req.body
+  if(creds.username && creds.password) {
+    db.login(creds.username)
+      .then(user => {
+        if(user.password && bcrypt.compareSync(creds.password, user.password)) {
+          res
+            .status(200)
+            json({message:'Welcome'})
+        } else {
+          res
+            .status(403)
+            .json({message: 'Failed to authenticate'})
+        }
+      })
+      .catch(() => {
+        res
+          .status(500)
+          .json({message: 'Failed to login'})
+      })
+  } else {
+    res
+      .status(404)
+      .json({message: 'username/password Missing'})
+  }
 }
 
 function getJokes(req, res) {
--- database/dbConfig.js
+++ database/dbConfig.js
@@ -1,5 +1,22 @@
 const knex = require('knex');
 
 const knexConfig = require('../knexfile.js');
+const db = knex(knexConfig.development);
 
-module.exports = knex(knexConfig.development);
+module.exports = {
+  getUser: (id) => {
+    if(id){
+      return db('users').where('id', id).first()
+    } else {
+      return db('users').select('id', 'username')
+    }
+  },
+
+  register: (creds) => {
+    db('users').insert('creds')
+  },
+
+  login: (username) => {
+    return db('users').where('username', username).first()
+  }
+};
